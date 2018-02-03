482b8be35460572a216193fbbb24ff7dde07a864
Thulasiram N <thulasiram.developer@gmail.com>
2018-02-03 19:38:58 +0530

    TestCommit

---
+++ DemoAPIRepository/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAPIRepository
+{
+
+    public class DemoRepository
+    {
+        private readonly DemoDBEntities _demoDBEntities = null;
+
+        public DemoRepository()
+        {
+            _demoDBEntities = new DemoDBEntities();
+        }
+
+        public string GetUserName()
+        {
+            return _demoDBEntities.Users.Select(o => o.UserName).FirstOrDefault();
+        }
+
+        public IEnumerable<string> GetAllUsers()
+        {
+            return _demoDBEntities.Users.Select(o => o.UserName).ToList();
+        }
+
+        public IEnumerable<User> GetAllUsersDetails()
+        {
+            return _demoDBEntities.Users;
+        }
+
+        public bool CreateUser(User user)
+        {
+            try
+            {
+                _demoDBEntities.Users.Add(user);
+                _demoDBEntities.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateUser(int userId,string username)
+        {
+            try
+            {
+                var user = _demoDBEntities.Users.FirstOrDefault(o => o.Id == userId);
+                user.UserName = username;
+                _demoDBEntities.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ DemoWebAPI/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using DemoAPIRepository;
+
+namespace DemoWebAPI.Controllers
+{
+    //[System.Web.Http.Authorize]
+    [System.Web.Http.RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        // GET: User
+       
+        public string GetFirstUserName()
+        {
+            DemoRepository repository = new DemoRepository();
+            return repository.GetUserName();
+        }
+        [System.Web.Http.Route("GetAllUsers")]
+        public IEnumerable<string> GetAllUsers()
+        {
+            DemoRepository repository = new DemoRepository();
+            return repository.GetAllUsers();
+        }
+        [System.Web.Http.Route("GetAllUsersDetails")]
+        public IEnumerable<User> GetAllUsersDetails()
+        {
+            DemoRepository repository = new DemoRepository();
+            return repository.GetAllUsersDetails();
+        }
+        public bool PostUser(string userName,bool isAdmin,string password)
+        {
+            DemoRepository repository = new DemoRepository();
+            User user = new User
+            {
+                UserName = userName,
+                IsAdmin = isAdmin,
+                Password = password
+            };
+            return repository.CreateUser(user);
+        }
+        public bool PutUser(int userId,string userName)
+        {
+            DemoRepository repository = new DemoRepository();
+          
+            return repository.UpdateUser(userId, userName);
+        }
+    }
+}
